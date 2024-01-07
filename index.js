b2e88b505b050d0a93a3e14662494d5755386244
nycamas <nycamas2017@gmail.com>
2024-01-07 05:21:35 +0100

    comit inicial

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import mongoose from 'mongoose'
-
 const Animal = mongoose.model('Animal', new mongoose.Schema({
   tipo: String,
   estado: String,
@@ -8,17 +7,24 @@ const Animal = mongoose.model('Animal', new mongoose.Schema({
 
 const app = express()
 
-mongoose.connect('mongodb://nico:password@monguito:27017/miapp?authSource=admin')
-
+//mongoose.connect('mongodb://angel:algjl@localhost:27017/miapp?authSource=admin')
+mongoose.connect('mongodb://angel:algjl@monguito:27017/laprimera?authSource=admin')
 app.get('/', async (_req, res) => {
-  console.log('listando... chanchitos...')
+  console.log('listando... guarrillos...o no, yo que se')
   const animales = await Animal.find();
   return res.send(animales)
 })
-app.get('/crear', async (_req, res) => {
-  console.log('creando...')
-  await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' })
-  return res.send('ok')
+
+app.get('/otro', async (_req, res) => {
+  console.log('que leche, vale creo otro')
+  await Animal.create({ tipo: 'tu prima la de doermana', estado: 'Cabreadamente_feliz' })
+  return res.send('ah... ok otra vez')
 })
 
-app.listen(3000, () => console.log('listening...'))
+app.get('/crear', async (_req, res) => {
+  console.log('que si      creando que viene fernando...')
+  await Animal.create({ tipo: 'Cochino_Jabalín', estado: 'Felizmente_cabreado' })
+  return res.send('ok uno solo un uno uno')
+})
+
+app.listen(3000, () => console.log('escuchando puerto 3000...'))
